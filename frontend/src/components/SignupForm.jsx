265674d8acaddf7bb09bf925037c34d59f09b5ed
student <dkel4578@naver.com>
2023-08-07 10:22:58 +0900

    오류 수정

---
+++ frontend/src/components/SignupForm.jsx
@@ -0,0 +1,16 @@
+import React, { useState, useRef, useContext, useEffect } from 'react';
+import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
+
+function SignupForm() {
+  const navigatge = useNavigate();
+
+  
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default SignupForm
