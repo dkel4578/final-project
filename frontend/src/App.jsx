74adf0cfec6110abf840b461d70c7fd8d629c911
student <dkel4578@naver.com>
2023-08-09 15:03:22 +0900

    1. 회운가입 페이지

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import Header from './layout/Header';
 import LoginPage from './pages/LoginPage';
 import HomePage from './pages/HomePage';
+import SignupPage from './pages/SignupPage';
 import { Routes, Route,  } from 'react-router-dom'
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage/>}/>
           <Route path="/login" element={<LoginPage/>}/>
+          <Route path="/signup" element={<SignupPage/>}/>
         </Routes>
     </>
   )
--- frontend/src/script/custom.js
+++ frontend/src/script/custom.js
@@ -1,5 +1,19 @@
 import $ from 'jquery';
 
+/* 눈모양 누르면  비밀번호 보이게 만들기 (전체 페이지)*/
+$(function(){
+  $('.toggle-eye').click(function(){
+    /* toggle 아이콘 모양 */
+    $(this).toggleClass('bi-eye')
+    /* toggle 인풋타입 */
+    let inputType = $(this).parent().children('input').attr('type');
+    if(inputType == 'password'){
+      $(this).parent().children('input').attr('type', 'text');
+    }else{
+      $(this).parent().children('input').attr('type', 'password');
+    }
+  })
+})
 /* 헤더 인클루드 */
 $(function(){
   $('.header-include').load('../html/header.html',function(){
