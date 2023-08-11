1347ef757ef0c527b77397a1545c0830cce81fab
student <dkel4578@naver.com>
2023-08-11 16:04:21 +0900

    프로필 이미지 업로드 진행중

--- backend/java-spring/src/main/java/com/example/gachi/config/WebSecurityConfig.java
+++ backend/java-spring/src/main/java/com/example/gachi/config/WebSecurityConfig.java
@@ -70,6 +70,7 @@ public class WebSecurityConfig {
                 .authorizeHttpRequests()
                 .requestMatchers(mvcMatcherBuilder.pattern("/api/**")).permitAll()
                 .requestMatchers(mvcMatcherBuilder.pattern("/oauth2/**")).permitAll()
+                .requestMatchers(mvcMatcherBuilder.pattern("/upload/**")).permitAll()
 //                .requestMatchers(mvcMatcherBuilder.pattern("/email-login","user/email-login","/EmailTemplate")).permitAll()
                 .requestMatchers(mvcMatcherBuilder.pattern("\"/\", \"/css/**\", \"/images/**\", \"/js/**\", \"/h2-console/**\", \"/api/**\"")).permitAll()
                 .anyRequest().authenticated()
---
+++ backend/java-spring/src/main/java/com/example/gachi/repository/ProfileImgRepository.java
@@ -0,0 +1,8 @@
+package com.example.gachi.repository;
+
+import com.example.gachi.model.ProfileImg;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ProfileImgRepository extends JpaRepository<ProfileImg, Long> {
+
+}
