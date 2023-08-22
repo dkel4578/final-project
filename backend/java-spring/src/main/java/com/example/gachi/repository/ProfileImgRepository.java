be5dc280a3d1a16e0028189f91b2448c7d06475d
dkel4578 <127079391+dkel4578@users.noreply.github.com>
2023-08-22 17:19:42 +0900

    채팅창 진행중222222

--- backend/java-spring/src/main/java/com/example/gachi/repository/ProfileImgRepository.java
+++ backend/java-spring/src/main/java/com/example/gachi/repository/ProfileImgRepository.java
@@ -3,13 +3,12 @@ package com.example.gachi.repository;
 import com.example.gachi.model.ProfileImg;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface ProfileImgRepository extends JpaRepository<ProfileImg, Long> {
 
     Optional<ProfileImg> findFirstByUserIdOrderByCreateAtDesc(Long id);
-
-    String findFirstImgSrcByUserIdOrderByCreateAtDesc(Long id);
 
 
 }
